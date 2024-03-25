4b0fdd899ba8f22bd32641cfd89df4b2f0e09c8d
Sebastian Alasia <72724740+TianSeb@users.noreply.github.com>
2024-03-25 00:38:26 -0300

    ios calculator

--- MyFirstApp/App.tsx
+++ MyFirstApp/App.tsx
@@ -3,8 +3,12 @@ import {HelloWorldScreen} from './src/presentation/screens/HelloWorldScreen';
 import {CounterScreen} from './src/presentation/screens/CounterScreen';
 import {PaperProvider} from 'react-native-paper';
 import {CounterM3Screen} from './src/presentation/screens/CounterM3Screen';
+import {BoxObjectModelScreen} from './src/presentation/screens/BoxObjectModelScreen';
 
 import IonIcon from 'react-native-vector-icons/Ionicons';
+import {DimensionScreen} from './src/presentation/screens/DimensionScreen';
+import { PositionScreen } from './src/presentation/screens/PositionScreen';
+import { FlexScreen } from './src/presentation/screens/FlexScreen';
 
 export const App = () => {
   return (
@@ -15,7 +19,11 @@ export const App = () => {
       <SafeAreaView style={{flex: 1}}>
         {/* <HelloWorldScreen name='Daiber' /> */}
         {/* <CounterScreen /> */}
-        <CounterM3Screen />
+        {/* <CounterM3Screen /> */}
+        {/* <BoxObjectModelScreen /> */}
+        {/* <DimensionScreen /> */}
+        {/* <PositionScreen /> */}
+        <FlexScreen />
       </SafeAreaView>
     </PaperProvider>
   );
--- iosCalculator/src/presentation/screens/CalculatorScreen.tsx
+++ iosCalculator/src/presentation/screens/CalculatorScreen.tsx
@@ -1,13 +1,45 @@
 import {Text, View} from 'react-native';
-import {styles} from '../../config/theme/app-theme';
+import {colors, styles} from '../../config/theme/app-theme';
+import {CalculatorButton} from '../components/CalculatorButton';
 
 export const CalculatorScreen = () => {
+  const printSomething = () => {
+    console.log('printSomething');
+  }
+
   return (
     <View style={styles.calculatorContainer}>
       <View style={{paddingHorizontal: 30, paddingBottom: 20}}>
         <Text style={styles.mainResult}>1500</Text>
         <Text style={styles.subResult}>15</Text>
       </View>
+
+      <View style={styles.row}>
+        <CalculatorButton
+          onPress={printSomething}
+          label="C"
+          blackText
+          color={colors.lightGray}
+        />
+        <CalculatorButton
+          onPress={printSomething}
+          label="+/-"
+          blackText
+          color={colors.lightGray}
+        />
+        <CalculatorButton
+          onPress={printSomething}
+          label="del"
+          blackText
+          color={colors.lightGray}
+        />
+        <CalculatorButton
+          onPress={printSomething}
+          label="÷"
+          color={colors.orange}
+        />
+      </View>
+      
     </View>
   );
 };
